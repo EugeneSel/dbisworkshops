a384a98100188ba1aa51b908c38b0d63135bd056
Eugene <youdjin.sel15@gmail.com>
2018-11-05 13:45:35 +0200

    Create task1.sql

---
+++ km-52/Sielskyi_Yevhenii/workshop1/sql/task1.sql
@@ -0,0 +1,7 @@
+/* 
+task 1
+Вставити дані з неіснуючим типом даних
+*/    
+
+INSERT INTO Data(data_address, excel_file_name, excel_file_path, link_url, database_name, database_path, data_type)
+    VALUES('134.3i', '', '', '', 'third', 'E:\', 'Complex');
