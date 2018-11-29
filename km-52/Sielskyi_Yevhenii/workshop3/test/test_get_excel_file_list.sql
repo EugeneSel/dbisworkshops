e677f64cf3de3a6445a9d6606d5fbd873f0d1d82
Eugene <youdjin.sel15@gmail.com>
2018-11-29 19:44:42 +0200

    Update test_get_excel_file_list.sql

--- km-52/Sielskyi_Yevhenii/workshop3/test/test_get_excel_file_list.sql
+++ km-52/Sielskyi_Yevhenii/workshop3/test/test_get_excel_file_list.sql
@@ -1,6 +1,14 @@
 # output_for_user.get_excel_file_list function test
 
 Declare
+    Type rowExcel is record(
+        excel_file_name "Excel file".excel_file_name%TYPE,
+        user_login "Excel file".user_login_fk%TYPE,
+        excel_file_size "Excel file".excel_file_size%TYPE,
+        excel_file_time "Excel file".excel_file_time%TYPE
+    );
+    
+    excel rowExcel;
 Begin
     DBMS_OUTPUT.put_line('File name, Author`s login, File size, File time');
     for rec in (Select * into excel from table(output_for_user.get_excel_file_list()))
